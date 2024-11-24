0c8b7dc3a738ebb97d2d4360b0bc7cc7e1498d4e
Andrei_Robert_Defta <157252875+Destroyer225200@users.noreply.github.com>
2024-11-25 01:18:13 +0200

    Update Page5_Alert.py

--- Page5_Alert.py
+++ Page5_Alert.py
@@ -1,19 +1,40 @@
-from PyQt5.QtWidgets import QWidget, QFormLayout, QLabel, QCheckBox, QSpinBox
-
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QGridLayout, QPushButton
+from PyQt5.QtCore import QTimer
 
 class Alert(QWidget):
     def __init__(self):
         super().__init__()
-        self.layout = QFormLayout()
+        self.layout = QVBoxLayout()
 
-        self.heat_pump_label = QLabel("Heat Pump Control:")
-        self.heat_pump_control = QCheckBox("Turn On/Off")
+        self.grid_layout = QGridLayout()
+        self.pumps = []
 
-        self.set_temperature_label = QLabel("Set Temperature (°C):")
-        self.set_temperature_input = QSpinBox()
-        self.set_temperature_input.setRange(0, 50)
+        for i in range(1, 6):
+            pump_label = QLabel(f"Pump {i}")
+            pump_status = QLabel()
+            pump_status.setStyleSheet("border: 1px solid black; background-color: green;")
+            self.grid_layout.addWidget(pump_label, i-1, 0)
+            self.grid_layout.addWidget(pump_status, i-1, 1)
+            self.pumps.append(pump_status)
 
-        self.layout.addRow(self.heat_pump_label, self.heat_pump_control)
-        self.layout.addRow(self.set_temperature_label, self.set_temperature_input)
-
+        self.layout.addLayout(self.grid_layout)
         self.setLayout(self.layout)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.check_pump_status)
+        self.timer.start(1000)  # Update every second
+
+    def check_pump_status(self):
+        for pump_status in self.pumps:
+            # Simulate pump status check
+            if self.is_pump_failing():
+                pump_status.setStyleSheet("border: 1px solid black; background-color: red;")
+                self.parentWidget().parentWidget().btn_page5.setStyleSheet("background-color: orange;")
+            else:
+                pump_status.setStyleSheet("border: 1px solid black; background-color: green;")
+                self.parentWidget().parentWidget().btn_page5.setStyleSheet("")
+
+    def is_pump_failing(self):
+        
+        import random
+        return random.choice([True, False])
